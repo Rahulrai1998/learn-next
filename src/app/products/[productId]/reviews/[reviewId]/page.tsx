fcf45bb0efdeabf166a8fa73c392d79b0c88f659
Rahul Kumar <rahulrai2822march@gmail.com>
2025-08-02 11:33:34 +0530

    dynamic routing

--- src/app/products/[productId]/reviews/[reviewId]/page.tsx
+++ src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -1,6 +1,9 @@
+import { notFound } from "next/navigation"
+
 export default async function Review({ params }: {
     params: Promise<{ reviewId: string, productId: string }>
 }) {
     const { reviewId, productId } = await params
+    if (parseInt(reviewId) > 1000) notFound()
     return <h1>Review {reviewId} of product {productId}</h1>
 }
